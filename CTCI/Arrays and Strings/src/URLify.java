aaada912751a177ff2261ea0dabf6790f349b155
Declan Michael McCabe <mccabed7tcd.ie>
2023-08-03 22:13:22 +0100

    Completed URLify in CTCI

--- CTCI/Arrays and Strings/src/URLify.java
+++ CTCI/Arrays and Strings/src/URLify.java
@@ -1,23 +1,37 @@
 /*
-    Problem:
+    Problem: implement an algorithm to replace the spaces in a string with "%20"
     Solution: incomplete
-    Tested:
-    Time Complexity:
-    Space Complexity:
+    Tested: yes
+    Time Complexity: O(n)
+    Space Complexity: O(n + c), where c is the amount of spaces in the string
 
     Notes:
-        +
+        + String builder makes strings behave like an arraylist
  */
+
+
 
 public class URLify
 {
 
-    public static void solution(String s){
-
+    public static String solution(String s){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) != ' '){
+                builder.append(s.charAt(i));
+            }
+            else
+            {
+                builder.append("%20");
+            }
+        }
+        return builder.toString();
     }
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println(solution("Hello World!"));
+        System.out.println(solution("     "));
     }
 }
