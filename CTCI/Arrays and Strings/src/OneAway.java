b27cea9050c493fbf95bf34e03ba9420d1416d41
Declan Michael McCabe <mccabed7@tcd.ie>
2023-08-07 20:51:26 +0100

    Completed OneAway in CTCI

--- CTCI/Arrays and Strings/src/OneAway.java
+++ CTCI/Arrays and Strings/src/OneAway.java
@@ -23,16 +23,23 @@ public class OneAway
      @return True if one or zero modifications were made, false otherwise
      */
     public static boolean solution(String s, String t){
-        boolean foundAdd = false;
-        boolean foundRemove = false;
-        boolean foundReplace = false;
+
+        if(s.length() == 1 && t.length() == 0) return true;
+
         for (int i = 0; i < s.length(); i++)
         {
             if(s.charAt(i) != t.charAt(i)){
+                //edge conditions not tested yet [when index is at end, empty strings, etc.]
                 //check add
+                if(s.substring(i).equals(t.substring(i + 1))) return true;
 
                 //check remove
+                if(s.substring(i + 1).equals(t.substring(i))) return true;
+
                 //check replace
+                if(s.substring(i + 1).equals(t.substring(i + 1))) return true;
+
+                return false;
             }
         }
         return true;
@@ -41,9 +48,9 @@ public class OneAway
 
     public static void main(String[] args)
     {
-        System.out.println("pale + ple");
-        System.out.println("pales + pale");
-        System.out.println("pale + bale");
-        System.out.println("pale + bake");
+        System.out.println("pale + ple" + solution("pale", "ple"));
+        System.out.println("pale + pasle" + solution("pale", "pasle"));
+        System.out.println("pale + bale" + solution("pale", "bale"));
+        System.out.println("pale + bake" + solution("pale", "bake"));
     }
 }
