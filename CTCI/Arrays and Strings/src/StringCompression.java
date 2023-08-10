4ed66e069c4004e79d7aac881b7f2754e6edd4b2
Declan Michael McCabe <mccabed7@tcd.ie>
2023-08-10 11:20:57 +0100

    Corrected, fully tested and commented String Compression

--- CTCI/Arrays and Strings/src/StringCompression.java
+++ CTCI/Arrays and Strings/src/StringCompression.java
@@ -1,18 +1,25 @@
 /*
     Problem: implement an algorithm that compresses Strings like  so: [aabccc] -> [a2bc3]
     Solution: Complete
-    Tested: no
-    Time Complexity:
-    Space Complexity:
+    Tested: Yes
+    Time Complexity: O(n) where n is the length of the string s
+    Space Complexity: O(n) where n is the length of the string s
 
     Notes:
-        +
+        +  This one was good practice.  I drew out an algorithm on paper, and implemented in code.
+        +  It was also a great exercise in debugging as there was a small logic error at the end
+                and I fixed it without using the line-by-line debugger.
  */
 
 
 public class StringCompression
 {
 
+    /**
+     * An algorithm that compresses strings like so: [aabccc] -> [a2bc3].
+     @param s The string to be compressed.
+     @return A compressed version of the string.
+     */
     public static String solution(String s){
         StringBuilder builder = new StringBuilder();
         int prevChar = -1;
@@ -22,22 +29,44 @@ public class StringCompression
             int sChar = s.charAt(i);
             if(prevChar == sChar) charCount++;
 
-            if((prevChar != sChar || i == s.length() - 1) && prevChar != -1){
+
+
+            if(prevChar != sChar && i != s.length() - 1){
                 builder.append((char)prevChar);
                 if(charCount > 1) builder.append(charCount);
 
                 charCount = 1;
             }
+            else if(i == s.length() - 1){
+                if(prevChar == sChar){
+                    builder.append((char)prevChar);
+                    if(charCount > 1) builder.append(charCount);
+                }
+                else{
+                    builder.append((char)prevChar);
+                    if(charCount > 1) builder.append(charCount);
+                    builder.append((char)sChar);
+                }
+
+            }
+
+
             prevChar = sChar;
 
         }
+
+        if(builder.length() > 0) builder.deleteCharAt(0);
         return builder.toString();
     }
 
 
     public static void main(String[] args)
     {
-        System.out.println(solution("aabccc"));
-        System.out.println(solution("Hello World!!!"));
+        System.out.println("aabccc -> " + solution("aabccc"));
+        System.out.println("Hello World! -> " + solution("Hello World!"));
+        System.out.println("Hello World!!! -> " + solution("Hello World!!!"));
+        System.out.println("{empty string} -> " + solution(""));
+        System.out.println("aaa -> " + solution("aaa"));
+        System.out.println("b -> " + solution("b"));
     }
 }
