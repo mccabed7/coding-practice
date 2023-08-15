dd5cc6e1eddfc71d794df54b69215a2cd56b6a6a
Declan Michael McCabe <mccabed7@tcd.ie>
2023-08-15 13:12:15 +0100

    Completed rotate matrix in CTCI

--- CTCI/Arrays and Strings/src/MatrixTools.java
+++ CTCI/Arrays and Strings/src/MatrixTools.java
@@ -1,6 +1,6 @@
 public class MatrixTools
 {
-    public static boolean IsSquare(int [][] matrix){
+    public static boolean isSquare(int [][] matrix){
 
         for (int i = 0; i < matrix.length; i++)
         {
@@ -9,7 +9,7 @@ public class MatrixTools
         return true;
     }
 
-    public static void PrintMatrix(int [][] matrix){
+    public static void printMatrix(int [][] matrix){
         for (int i = 0; i < matrix.length; i++)
         {
             for (int j = 0; j < matrix[i].length; j++)
--- CTCI/Arrays and Strings/src/RotateMatrix.java
+++ CTCI/Arrays and Strings/src/RotateMatrix.java
@@ -15,14 +15,40 @@
 public class RotateMatrix
 {
 
-    public static void solution(){
+    public static boolean solution(int[][] matrix){
+        if(!MatrixTools.isSquare(matrix) || matrix.length == 0)  return false;
+        for (int i = 0; i < matrix.length / 2; i++)
+        {
+            int end = matrix.length -1 - i;
+            for (int j = i; j < end; j++)
+            {
+                int temp =  matrix[i+j][i];  //temp copy of top left
 
+                matrix[i+j][i] = matrix[i][end-j];
+                matrix[i][end-j]  =  matrix[end-j][end];
+                matrix[end-j][end] = matrix[end][i + j];
+                matrix[end][i + j] = temp;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args)
     {
-        int[][] matrix1 = {{1,1,1}, {1,1,1}, {1,1,1}};
-        MatrixTools.PrintMatrix(matrix1);
-        System.out.println("Hello World!");
+        int[][] matrix1 = {{1,2,3}, {4,5,6}, {7,8,9}};
+        MatrixTools.printMatrix(matrix1);
+        solution(matrix1);
+        MatrixTools.printMatrix(matrix1);
+
+        int[][] matrix2 =  {{1,2,3,55}, {4,5,6,77}, {7,8,9,33}, {4,3,2,1}};
+        MatrixTools.printMatrix(matrix2);
+        solution(matrix2);
+        MatrixTools.printMatrix(matrix2);
+
+        int[][] matrix3 =  {{5}};
+        MatrixTools.printMatrix(matrix3);
+        solution(matrix3);
+        MatrixTools.printMatrix(matrix3);
     }
 }
