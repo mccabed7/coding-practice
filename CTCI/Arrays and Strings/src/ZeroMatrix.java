e7336bbe3f6531cdb2db86fed428bf2dd3a954b3
Declan Michael McCabe <mccabed7@tcd.ie>
2023-08-16 22:11:11 +0100

    Completed  and tested ZeroMatrix in CTCI

--- CTCI/Arrays and Strings/src/ZeroMatrix.java
+++ CTCI/Arrays and Strings/src/ZeroMatrix.java
@@ -1,17 +1,19 @@
 /*
     Problem: implement an algorithm to find the zeroes in a matrix, and set their row and column to all zeroes
-    Solution: incomplete
-    Tested: no
-    Time Complexity: Expected O(M * N)
-    Space Complexity: Expected O(1)
+    Solution: Complete
+    Tested: Yes
+    Time Complexity: O(M * N)
+    Space Complexity: O(1)
 
     Notes:
-        +
+        + I initially thought to  use a seperate array for storing which rows and columns  needed to be zeroed.
+        + However using the matrix  itself  worked fine.
  */
 
 public class ZeroMatrix
 {
     public static void solution(int[][] matrix){
+        boolean rowZero = false;
         for (int i = 0; i < matrix.length; i++)
         {
             for (int j = 0; j < matrix[i].length; j++)
@@ -19,11 +21,41 @@ public class ZeroMatrix
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
+
+                    if(i == 0){
+                        rowZero = true;
+                    }
                 }
             }
         }
 
-        
+        for (int i = 1; i < matrix.length; i++)
+        {
+            if(matrix[i][0] == 0){
+                for (int j = 0; j < matrix[i].length; j++)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix[0].length; i++)
+        {
+            if(matrix[0][i] == 0){
+                for (int j = 0; j < matrix.length; j++)
+                {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        if(rowZero){
+            for (int i = 0; i < matrix[0].length; i++)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+
     }
 
     public static void main(String[] args)
