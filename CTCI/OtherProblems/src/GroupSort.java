007d61ac01080e0128d3daad7b0f7195dee8c9e3
Declan Michael McCabe <mccabed7@tcd.ie>
2023-08-16 22:21:34 +0100

    Added other problems project in CTCI, and GroupSort algorithm

---
+++ CTCI/OtherProblems/src/GroupSort.java
@@ -0,0 +1,37 @@
+/*
+    Problem:
+    Source: Amazon practice interview question
+    Solution:
+    Tested:
+    Time Complexity:
+    Space Complexity:
+
+    Explanation of implementation:
+
+    Notes:
+        +
+
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupSort
+{
+
+    public static List<List<Integer>> solution(List<Integer> arr){
+        return null;
+    }
+
+
+    public static void main(String[] args)
+    {
+        List<Integer> test1 = Arrays.asList(1,3,34,4,4,3,2,23,43,4,2,2,1,1,2,3,3);
+        for (int i = 0; i < test1.size(); i++)
+        {
+            System.out.println(test1.get(i));
+        }
+        System.out.println("Hello world!");
+    }
+}
