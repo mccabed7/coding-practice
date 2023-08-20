8e4650f701795b26ce768c082e441503dfcc9422
Declan Michael McCabe <mccabed7@tcd.ie>
2023-08-20 01:09:17 +0100

    Partially completed MinimumWildcard in CTCI

--- CTCI/OtherProblems/src/MinimumWildcard.java
+++ CTCI/OtherProblems/src/MinimumWildcard.java
@@ -15,10 +15,36 @@
 
 import java.util.List;
 
+
 public class MinimumWildcard
 {
 
     public static int solution(List<String> patterns){
-        
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < patterns.get(0).length(); i++)
+        {
+            char firstChar = patterns.get(0).charAt(i);
+            boolean validChar = true;
+            for (int j = 0; j < patterns.size(); j++)
+            {
+                if(patterns.get(j).charAt(i) != firstChar && patterns.get(j).charAt(i) != '?') validChar = false;
+            }
+            if(validChar){
+                builder.append(firstChar);
+            }
+            else{
+                builder.append('?');
+            }
+        }
+
+        String wildCard = builder.toString();
+        int count = 0;
+        for (int i = 0; i < wildCard.length(); i++)
+        {
+            if(wildCard.charAt(i) =='?') count++;
+        }
+
+        return count;
     }
 }
