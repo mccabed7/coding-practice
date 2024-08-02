944f9985c3e5136744dab0d631a610845464aee2
Declan <mccabed7@tcd.ie>
2024-08-02 23:20:45 +0100

    Completed NthToLast in ctci

--- CTCI/LinkedLists/src/NthToLast.java
+++ CTCI/LinkedLists/src/NthToLast.java
@@ -1,27 +1,38 @@
 /*
     Problem:  Return the nth to last element of a singly linked list
-    Solution:  Incomplete
-    Tested:  No
-    Time Complexity:
-    Space Complexity:
+    Solution:  Complete
+    Tested:  Yes
+    Time Complexity:  O(n)
+    Space Complexity: O(1)
 
     Notes:
-        +
-        +
-        +
+        + An example where iteration is far superior to recursion
+        + Another two-pointer approach, both going in the same direction
+        + This was a very simple problem
  */
 
 public class NthToLast {
 
 
     /**
-     *
+     *  A function to return the nth last node in a linked list
      @param node head of the list
+     @return The n-th last node, or null if index out of bounds
      */
-    public static Node nthToLast(Node node, int i){
-        return null;
+    public static Node nthToLast(Node node, int n){
+        if(n == 0) return null;
+        Node start = node;  //new 'safe' pointer to start node
+        Node leadNode = node;
+        for (int i = 0; i < n - 1; i++){ //n = ((how far ahead we go) - (1))
+            if(leadNode.next == null) return null;
+            leadNode = leadNode.next;
+        }
+        while (leadNode.next != null){
+            leadNode = leadNode.next;
+            start = start.next;
+        }
+        return start;
     }
-
 
 
     public static void main(String[] args) {
@@ -37,5 +48,6 @@ public class NthToLast {
         head.next.next.next.next.next.next.next.next.next = new Node(1111);
         head.next.next.next.next.next.next.next.next.next.next = new Node(1661);
         head.printList();
+        System.out.println(nthToLast(head, 3).data);
     }
 }
